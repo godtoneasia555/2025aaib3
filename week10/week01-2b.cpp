6ff31dc273cbf2a8b8cc7418a7873e0e4a9c018e
godtoneasia555 <jennie940914@gmail.com>
2025-11-12 11:50:29 +0800

    Add files via upload

---
+++ week10/week01-2b.cpp
@@ -0,0 +1,5 @@
+# week01-2b.py leetcode 學習計畫 python 版本1 for 迴圈
+# leetCode 28.  Find the Index of the First Occurrence in a String
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        return haystack.find(needle) #只有一行
